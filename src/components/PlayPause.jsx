d5e2ecfd17672528fb12a47e2b82c525c6994a2c
whiHak <whiHak>
2023-11-06 16:47:56 +0300

    added play pause icon

---
+++ src/components/PlayPause.jsx
@@ -0,0 +1,23 @@
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+
+const PlayPause = ({
+  song,
+  isPlaying,
+  activeSong,
+  handlePause,
+  handlePlay,
+}) => (isPlaying && activeSong?.name === song.name)?(
+  <FaPauseCircle 
+    size={35}
+    className=" text-gray-300 "
+    onClick={handlePause}
+  />
+):(
+  <FaPlayCircle 
+    size={35}
+    className=" text-gray-300 "
+    onClick={handlePlay}
+  />
+)
+
+export default PlayPause;
