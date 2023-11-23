09cc04227ca5845dcca6ad97cedf982b878ca520
whiHak <whiHak>
2023-11-23 16:08:19 +0300

    modified styles

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -24,7 +24,7 @@ const SongCard = ({ song, key, i, isPlaying, activeSong, data }) => {
     dispatch(playPause(true))
   };
   return (
-    <div className=" flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className=" flex flex-col w-[230px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className=" relative w-full h-56 group ">
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
